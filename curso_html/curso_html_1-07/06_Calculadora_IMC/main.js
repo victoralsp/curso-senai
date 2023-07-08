7a3a7de74979cbe3dbc86e63a8e53d6789a887a7
victoralsp <131179996+victoralsp@users.noreply.github.com>
2023-07-08 14:28:33 -0300

    Finalizada calculadora de IMC

---
+++ curso_html/curso_html_1-07/06_Calculadora_IMC/main.js
@@ -0,0 +1,35 @@
+const resultado = document.querySelector('#resultado')
+const altura = document.querySelector('#altura')
+const peso = document.querySelector('#peso')
+
+
+// Função para calcular o IMC
+const calcularIMC = () => {
+    if (altura.value !== '' && peso.value !== '') {
+        // Fórmula do IMC
+         const imc = (peso.value / (altura.value * altura.value)).toFixed(2)
+        let classificacao = ''
+
+        if (imc < 18.5) {
+            classificacao = 'Abaixo do peso!'
+        }
+        else if (imc < 25) {
+            classificacao = 'Peso Normal!'
+        }
+        else if (imc < 30) {
+            classificacao = 'A cima do peso!'
+        }
+        else if (imc < 40) {
+            classificacao = 'Obesidade grau II'
+        }
+        else {
+            classificacao = 'Obesidade grau III'
+        }
+
+        // Exibir resultado na tela 
+        resultado.innerHTML = `IMC: ${imc} (${classificacao})`
+        }
+        else {
+            resultado.innerHTML = 'Preencha os campos!'
+        }
+}
