2ecd12c94b688f42f65d31e897a7989b24e2d8ad
victoralsp <131179996+victoralsp@users.noreply.github.com>
2023-07-08 16:19:39 -0300

    Fazendo relógio digital com JS

---
+++ curso_html/curso_html_1-07/07_Relogio_analogico/main.js
@@ -0,0 +1,12 @@
+let hr = document.getElementById('hora')
+let min = document.getElementById('min')
+let seg = document.getElementById('seg')
+
+function displayHora() {
+    let date = new Date()
+
+    // Buscar horas, minutos e segundos a partir da data 
+    let hh = date.getHours();
+    let mm = date.getMinutes();
+    let ss = date.getSeconds();
+}
