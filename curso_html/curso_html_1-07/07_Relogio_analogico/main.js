8652b7a1efd0309973274d4f857a80dd0eb034d8
victoralsp <131179996+victoralsp@users.noreply.github.com>
2023-07-15 10:15:10 -0300

    cronometro

--- curso_html/curso_html_1-07/07_Relogio_analogico/main.js
+++ curso_html/curso_html_1-07/07_Relogio_analogico/main.js
@@ -9,4 +9,14 @@ function displayHora() {
     let hh = date.getHours();
     let mm = date.getMinutes();
     let ss = date.getSeconds();
+
+    let hRotation = 30 * hh + mm / 2
+    let mRotation = 6 * mm
+    let sRotation = 6 * ss
+
+    hr.style.transform = `rotate(${hRotation}deg)`
+    min.style.transform = `rotate(${mRotation}deg)`
+    seg.style.transform = `rotate(${sRotation}deg)`
 }
+
+setInterval(displayHora, 1000)
